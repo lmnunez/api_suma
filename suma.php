ea35853b6861543f68a1ccf4c4733cff4eb28998
lmnunez <lmnunezcastro@gmail.com>
2021-10-18 22:58:15 -0400

    Update suma.php

--- suma.php
+++ suma.php
@@ -1,5 +1,5 @@
 <?php 
-$respose = array();
+$response = array();
 if(isset($_POST['suma'])){
 	$valor1 = $_POST['valor1'];
 	$valor2 = $_POST['valor2'];
@@ -10,4 +10,4 @@ if(isset($_POST['suma'])){
 }else{
 	$response['success'] = false;
 }
-echo(json_encode($respose));
+echo(json_encode($response));
